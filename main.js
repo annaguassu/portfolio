0669bb23b7a932cdc0546b52d07e0dc8694bb652
Ana Beatriz Guassu <79941051+annaguassu@users.noreply.github.com>
2022-02-03 22:59:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
+//mudar tema
 const chk = document.getElementById('chk')
 
 chk.addEventListener('change', () =>{
     document.body.classList.toggle('dark')
 })
+
+//voltar para o topo
+const topinho = document.getElementById('topinho')
+
+function scrollToTop(){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
