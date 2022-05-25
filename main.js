f1a258901d2f8781ba289f8a188a9ae1b6fc2f50
Ana Beatriz Guassu <79941051+annaguassu@users.noreply.github.com>
2022-05-25 07:17:34 -0300

    Add files via upload

--- main.js
+++
@@ -1,8 +0,0 @@
-const sr = ScrollReveal({reset: true})
-
-sr.reveal(".scrollup", {
-    origin: "bottom", //Para ele surgir de cima para baixo.
-    duration: 2000, // A duração da animação em ms
-    delay: 300, // O delay para a animação acontecer
-    reset: true // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
-  });
